1df36342839d0f7e715a67d9e4f44c55087da4ac
Melvin Carvalho <melvincarvalho@gmail.com>
2026-04-25 06:44:05 +0200

    Render heading from local name, not rdfs:label
    Bumps to 0.0.5.
    
    Refs #5

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ function Category(props) {
 
   return html`
     <div>
-      <h1 id="${props.id}" onClick=${handleHeadingClick}>${props.label}</h1>
+      <h1 id="${props.id}" onClick=${handleHeadingClick}>${props.id}</h1>
       <p>Comment: ${props.comment}</p>
       <p>Term status: ${props.termStatus}</p>
     </div>
